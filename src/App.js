499fe66b02543aade3d0141f27a2fdedbc85a633
mlee51 <mlee@lavamap.com>
2022-03-21 11:52:24 -0400

    Added draco compression, some clean up

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import React, { useRef, useState, Suspense, state, useEffect } from 'react'
-import { Canvas, useFrame, useLoader, Vector3 } from '@react-three/fiber'
+import React, { useRef, Suspense, useEffect } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 import * as THREE from "three";
-import Model from './Model'
-
-
-
-function Box(props) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef()
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
-  // Return the view, these are regular Threejs elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-}
+import Model from './Dmodel'
 
 var trackCurve = new THREE.CatmullRomCurve3([
   new THREE.Vector3(4.26, 1.17, 12.29),
@@ -37,13 +12,11 @@ var trackCurve = new THREE.CatmullRomCurve3([
 
 var camPosIndex = 1;
 var scrollDist = 1;
-//var target = [0,0,0]
 
 function Dolly() {
   useFrame((state) => {
     camPosIndex = THREE.MathUtils.lerp(camPosIndex, scrollDist * 30, 0.05);
     const camPos = trackCurve.getPoint(camPosIndex / 1000)
-    //state.camera.position.z = 40 + Math.sin(state.clock.getElapsedTime()) * 20
     state.camera.position.set(camPos.x, camPos.y, camPos.z)
     state.camera.lookAt(4, 0, 0)
     state.camera.updateProjectionMatrix()
